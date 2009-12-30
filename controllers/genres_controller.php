2ef710868b004f033a36b2c9311fa514d3cc61e5
Cary Lee <cary@carysmac.local>
2009-12-30 00:09:46 -0500

    Trying out a genres controller for autocomplete

---
+++ controllers/genres_controller.php
@@ -0,0 +1,14 @@
+<?php
+class GenresController extends AppController {
+
+	var $name = 'Genres';
+	var $helpers = array('Html', 'Ajax', 'Form', 'Player');
+  var $uses = array('Response');
+
+  function autoComplete() {
+    $genres = $this->Response->find('all', array(
+      'fields' => array('genre')));
+    $this->set('genres', $genres);
+    $this->layout = 'ajax';
+  }
+}
