c1f0e8d457ab6a8209dbd486f3ff385c0ec275cd
Cary Lee <carylee@gmail.com>
2010-01-27 23:03:06 -0600

    Updated gitignore

--- webroot/scripts/webcam.php
+++ webroot/scripts/webcam.php
@@ -4,7 +4,7 @@
 /* Receives JPEG webcam submission and saves to local file. */
 /* Make sure your directory has permission to write files as your web server user! */
 
-$filename = 'files/' . date('YmdHis') . '.jpg';
+$filename = 'images/' . date('YmdHis') . '.jpg';
 $result = file_put_contents( $filename, file_get_contents('php://input') );
 if (!$result) {
 	print "ERROR: Failed to write data to $filename, check permissions\n";
